23fc43c834c037584065fd0fdf203ce8e863a91b
yurockx <71849449+yurockx@users.noreply.github.com>
2025-02-26 03:00:56 +0200

    Create 3 v2.py

---
+++ Module_3/3 v2.py
@@ -0,0 +1,11 @@
+import re
+def normalize_phone(phone_number):
+    full_prefix = "+38"
+    short_prefix = "+"
+    pattern = r"[^0-9]"
+    replacement = ""
+    numbers_only = re.sub(pattern, replacement, phone_number)
+    if numbers_only.startswith("0"):
+        return str(full_prefix + numbers_only)
+    else:
+        return str(short_prefix + numbers_only)
