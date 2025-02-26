f533e68c7f8fcb7a7c7aae62d7dc8701082a26b8
yurockx <71849449+yurockx@users.noreply.github.com>
2025-02-27 01:08:50 +0200

    small update in numb before today

--- Module_3/Number_days_before_today.py
+++ Module_3/Number_days_before_today.py
@@ -1,5 +1,4 @@
-from datetime import datetime, date
-today = date.today()
+from datetime import datetime
 def get_days_from_today(date):
     date = datetime.strptime(date, "%Y-%m-%d").date()
-    return (today - date).days
+    return (date.today() - date).days
