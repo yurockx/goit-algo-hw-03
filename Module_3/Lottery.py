dc10e4d10365f8ad8d2cba413c94dd6b2c1d3536
yurockx <71849449+yurockx@users.noreply.github.com>
2025-02-27 22:44:39 +0200

    the second review/adjustement

--- Module_3/Lottery.py
+++ Module_3/Lottery.py
@@ -1,7 +1,7 @@
 import random
 def get_numbers_ticket(min, max, quantity):
     empty_set = set()
-    if min >= 1 and max <= 1000 and min < max:
+    if min >= 1 and max <= 1000 and min < max and max - min > quantity:
         while len(empty_set) < quantity:
             empty_set.add(random.randrange(min, max))
         result = list(empty_set)
--- Module_3/Number_days_before_today.py
+++ Module_3/Number_days_before_today.py
@@ -1,4 +1,7 @@
 from datetime import datetime
 def get_days_from_today(date):
-    date = datetime.strptime(date, "%Y-%m-%d").date()
-    return (date.today() - date).days
+    try:
+        date = datetime.strptime(date, "%Y-%m-%d").date()
+        return (date.today() - date).days
+    except ValueError:
+        return "Invalid date format! Please enter the date in the following format: YYYY-MM-DD"
